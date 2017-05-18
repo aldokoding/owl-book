3485bfa3a7be4dfb86aaaa4ffa26d220f82cc43e
aldokoding <aldo.donal@gmail.com>
2017-05-18 14:40:30 +0700

    Signed-off-by: aldokoding <aldo.donal@gmail.com>

--- conn.php
+++ conn.php
@@ -2,6 +2,8 @@
 mysql_connect("localhost","root","");
 mysql_select_db("perpusweb");
 
+// Coba commit
+
 //fungsi format rupiah 
 /**function format_rupiah($rp) {
 	$hasil = "Rp." . number_format($rp, 0, "", ".") . ",00";
